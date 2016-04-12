c2a9729a780d4e86ada13cde4d6c774bceb39655
oasisbwin <1655589127@qq.com>
2016-04-12 22:59:03 +0800

    First Commit

--- js/main.js
+++ js/main.js
@@ -30,31 +30,67 @@ addEvent('click', window, function(e){
 
 /* 拖拽功能 */
 var modalHeader=$(".modal-header");
-var mouseOffsetX= 0,mouseOffsetY= 0,isDragble=false;
+var dialogObj, onMoveStartId, mousePos={x:0,y:0};
 
-addEvent('mousedown',modalHeader ,function(e){
-    var e = e||window.event;
-    stopDefault(e);
-    mouseOffsetX= e.pageX-modalHeader.offsetLeft;   //鼠标相对于浮层的位置
-    mouseOffsetY= e.pageY-modalHeader.offsetTop;    //鼠标相对于浮层的位置
-    isDragble=true;
-});
-addEvent('mouseup', modal , function(e){
-    isDragble=false;
-});
-addEvent('mousemove', modal , function(e){
-    var e = e||window.event;
-    stopDefault(e);
-    var mouseX= e.pageX;
-    var mouseY= e.pageY;
-    var moveX= 0,moveY=0;
-    if(isDragble){
-        alert(moveX);
-        var maxX=document.documentElement.clientWidth-modal.offsetWidth;
-        var maxY=document.documentElement.clientHeight-modal.offsetHeight;
-        moveX=Math.min(maxX,Math.max(0,moveX));
-        moveY=Math.min(maxY,Math.max(0,moveY));
-        modal.style.left=moveX+'px';
-        modal.style.top=moveY+'px';
+//addEvent('mousedown',modalHeader ,function(e){
+//    var e = e||window.event;
+//    stopDefault(e);
+//    mouseOffsetX= e.pageX-modalHeader.offsetLeft;   //鼠标相对于浮层的位置
+//    mouseOffsetY= e.pageY-modalHeader.offsetTop;    //鼠标相对于浮层的位置
+//    isDragble=true;
+//});
+//addEvent('mouseup', document , function(e){
+//    dialogObj=false;
+//    clearInterval(onMoveStartId);
+//});
+
+function dialogInitial(dragEle,moveEle){
+    var obj={};
+    obj.dragElement=dragEle;
+    obj.moveELement=moveEle;
+
+    obj.mouseOffsetLeft=0;
+    obj.mouseOffsetTop=0;
+
+    addEvent('mousedown',obj.dragElement,function(e){
+        var e=e||window.event;
+        dialogObj=obj;
+        obj.mouseOffsetLeft= e.pageX-obj.moveELement.offsetLeft;
+        obj.mouseOffsetTop= e.pageY-obj.moveELement.offsetTop;
+
+        onMoveStartId=setInterval(onMoveStart,10);
+        return false;
+    });
+    return obj;
+}
+
+document.onmouseup=function(e){
+    dialogObj=false;
+    clearInterval(onMoveStartId);
+};
+
+
+document.onmousemove=function(e){
+   var e=e||window.event;
+    mousePos.x= e.clientX;
+    mousePos.y= e.clientY;
+
+    e.stopPropagation&& e.stopPropagation();
+    e.cancelBubble=true;
+    e=this.originalEvent;
+    e&&(e.preventDefault? e.preventDefault(): e.returnValue=false);
+    document.body.style.MozUserSelect='none';
+};
+
+function onMoveStart(){
+    var obj=dialogObj;
+    if(obj){
+        var maxX=document.documentElement.clientWidth-obj.moveELement.offsetWidth;
+        var maxY=document.documentElement.clientHeight-obj.moveELement.offsetHeight;
+
+        obj.moveELement.style.left=Math.min(Math.max((mousePos.x-obj.mouseOffsetLeft),0),maxX)+'px';
+        obj.moveELement.style.top=Math.min(Math.max((mousePos.y-obj.mouseOffsetTop),0),maxY)+'px';
     }
-});
+}
+
+dialogInitial(modalHeader,modalContent);
